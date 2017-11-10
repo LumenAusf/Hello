3544c87acf0579a2dc9a14f92fc94cd66ae5b216
LumenAusf <demandeat@gmail.com>
2017-11-11 00:49:24 +0300

    init

---
+++ src/hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 10 нояб. 2017 г.
+ *      Author: lumenausf
+ */
+#include <iostream>
+
+int main (){
+	std::cout << "Hello World! And Other" << std::endl;
+	return std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+
