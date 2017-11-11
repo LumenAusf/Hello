2fef483b1884ea0177cbc8d94c42010aa03c4846
LumenAusf <demandeat@gmail.com>
2017-11-11 10:51:58 +0300

    remove

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 10 нояб. 2017 г.
+ *      Author: lumenausf
+ */
+#include <cstdlib>
+#include <iostream>
+
+int main(int, char**) {
+  std::cout << "Hello World! And Other" << std::endl;
+  return std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
